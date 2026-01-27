02973928f3fa96a9618c82f4cffe40d25234abd0
THEC1-zc <fabio.viscarelli@gmail.com>
2026-01-27 14:49:07 +0100

    Update chickens.js

--- api/chickens.js
+++ api/chickens.js
@@ -1,12 +1,9 @@
-export default function handler(req, res) {
-const apiKey = process.env.NFTSCAN_API_KEY;
-
-```
-return res.status(200).json({
-    message: 'Function works!',
-    hasApiKey: !!apiKey,
-    keyPreview: apiKey ? apiKey.substring(0, 4) + '...' : 'NOT SET'
-});
-```
-
-}
+module.exports = (req, res) => {
+    const apiKey = process.env.NFTSCAN_API_KEY;
+    
+    res.status(200).json({
+        message: 'Function works!',
+        hasApiKey: !!apiKey,
+        keyPreview: apiKey ? apiKey.substring(0, 4) + '...' : 'NOT SET'
+    });
+};
